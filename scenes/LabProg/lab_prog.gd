9326578cf62948c12514a0b714ab20c5bd7f2a1a
João Victor <js5283283@gmail.com>
2025-10-05 21:36:15 -0300

    Finalização do LabProg e Início do Hall da Fama

--- scenes/LabProg/lab_prog.gd
+++ scenes/LabProg/lab_prog.gd
@@ -4,11 +4,16 @@ signal new_area_entered(text : String, image : Texture2D)
 signal disable_previous
 
 @export var camera : Camera2D
+@export var bugs_scene : PackedScene
 
 @export var title_area : String
 @export var image : Texture2D
 @export var collisionArea : CollisionShape2D
 @export var next_wall_collision : CollisionShape2D
+
+@onready var spawn_points: Node2D = $SpawnPoints
+
+var bugs_counter : int = 6
 
 func enter_new_area() -> void:
 	emit_signal("new_area_entered", title_area, image)
@@ -21,3 +26,20 @@ func _on_area_detect_body_entered(body: Node2D) -> void:
 	enter_new_area()
 	var tween : Tween = create_tween()
 	tween.tween_property(camera, "global_position:x", collisionArea.global_position.x, 0.3)
+
+func _on_npc_ended_talking() -> void:
+	for child in spawn_points.get_children():
+		var bug_instance : CharacterBody2D = bugs_scene.instantiate()
+		bug_instance.position = child.position
+		bug_instance.connect("mob_death", _on_mob_death)
+		add_child(bug_instance)
+		await get_tree().create_timer(1.0).timeout
+
+func _on_mob_death() -> void:
+	bugs_counter -= 1
+	
+	if bugs_counter <= 0:
+		next_wall_collision.set_deferred("disabled", true)
+
+func _on_hall_of_fame_disable_previous() -> void:
+	next_wall_collision.set_deferred("disabled", false)
