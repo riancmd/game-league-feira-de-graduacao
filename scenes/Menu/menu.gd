b473d6f31aa0816f4edbfc1ed89388bcb1801078
João Anunciação <js5283283@gmail.com>
2026-09-23 21:48:34 -0300

    Add grab focus to apresentation button, fix tic tac sound after clock death

--- scenes/Menu/menu.gd
+++ scenes/Menu/menu.gd
@@ -1,4 +1,9 @@
 extends Control
 
+@export var start_button : Button
+
+func _ready() -> void:
+	start_button.grab_focus()
+
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Game/game.tscn")
--- scenes/enemies/clock.gd
+++ scenes/enemies/clock.gd
@@ -25,12 +25,13 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	_on_clock_timer_timeout()
 
 func _on_clock_timer_timeout() -> void:
+	if is_dead:
+		clock_timer.stop()
+		return
+		
 	emit_signal("tic_tac", is_tic_blue)
 	is_tic_blue = not is_tic_blue
-	if is_tic_blue:
-		SfxManager.play_sfx(SfxManager.TIC)
-	else:
-		SfxManager.play_sfx(SfxManager.TAC)
+	SfxManager.play_sfx(SfxManager.TIC if is_tic_blue else SfxManager.TAC)
 	clock_timer.start()
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
